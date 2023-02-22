9a60a40cb3925749b77c2cd79fcb900d65dce84e
Admin <Admin@DESKTOP-ILJG79A>
2023-02-22 13:45:44 +0530

    First commit

---
+++ Operation/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a=10 , b=20;
+		System.out.println("add :" + (a+b));
+		System.out.println("sub :" + (a-b));
+		System.out.println("mult :" + (a*b));
+		System.out.println("div :" + (b/a));
+		System.out.println("rem :" + (b/a));
+
+	}
+
+}
