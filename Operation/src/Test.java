14251bf8d657e41fc4249254d28ef9b477eda628
Admin <Admin@DESKTOP-ILJG79A>
2023-02-22 13:53:46 +0530

    add one variable

--- Operation/src/Test.java
+++ Operation/src/Test.java
@@ -2,12 +2,13 @@
 public class Test {
 
 	public static void main(String[] args) {
-		int a=10 , b=20;
+		int a=10 ,  b=20 , c=2 ;
 		System.out.println("add :" + (a+b));
 		System.out.println("sub :" + (a-b));
 		System.out.println("mult :" + (a*b));
 		System.out.println("div :" + (b/a));
 		System.out.println("rem :" + (b/a));
+		System.out.println("add :" + (a+b+c));
 
 	}
 
